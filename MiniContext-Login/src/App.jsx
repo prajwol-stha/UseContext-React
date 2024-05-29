04318806d0e3371906a4f9d85c20f9c17342d1d1
Prajwol Shrestha <prajwol.stha777@gmail.com>
2024-05-29 13:39:09 +0545

    first

---
+++ MiniContext-Login/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <UserContextProvider>
+        <h1>React user context api</h1>
+        <Login/>
+        <Profile/>
+      </UserContextProvider>
+    </>
+  )
+}
+
+export default App
